d9dcee5435d28dd9dff32d4153a702ad810fe75b
Sayi <sonny.sayi@gmail.com>
2024-10-27 10:48:36 +0300

    updates

--- server.js
+++ server.js
@@ -34,11 +34,11 @@ if (!mongoURI) {
 }
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/carDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+// mongoose.connect('mongodb://localhost:27017/carDB', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.error('Error connecting to MongoDB:', err));
 
 
   mongoose.connect(mongoURI, {
